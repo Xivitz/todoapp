3448c5eb545474599b0b6dcb469c3d7b4aedf4c9
Matheus Dias <diamatheus@gmail.com>
2020-01-15 19:00:51 +0000

    Creating the base project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { Todo } from './models/todo.models';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', // isso vira uma tag html => <app-root>
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todoapp';
+  public todos: Todo[] = [];
+  public title: string = 'Minhas Tarefas';
+
+  constructor() {
+    this.todos.push(new Todo(1, 'Passear com o dog', false));
+    this.todos.push(new Todo(2, 'Comprar algo', false));
+    this.todos.push(new Todo(3, 'Ir ao ginásio', true));
+  }  
 }
